4da83ce5d4be92071ed3642bc07ae43b3c6ee7de
karkeyan2004 <karkeyan2004@gmail.com>
2023-01-19 14:50:31 +0530

    new file added

---
+++ add2num.js
@@ -0,0 +1,6 @@
+function add(){
+    let a=document.getElementById("a").value;
+     let b=document.getElementById("b").value;
+     let c=parseInt(a)+parseInt(b);//parseInt
+     document.getElementById("c").value=c;
+  }
